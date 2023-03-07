d3f753a01d7ef5caf4c8cf7f460cc7f8b5739f43
Евгений Красовский <semmitwinsboss@gmail.com>
2023-03-07 16:51:19 +0300

    CommentsViewModel done

---
+++ knigaWEB/Models/CommentsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+ 
+namespace knigaWEB.Models
+{
+    public class Comment
+    {
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+ 
+        [Required]
+        [DataType(DataType.DateTime)]
+        public string Date { get; set; }
+ 
+        public int CommentId { get; set; }
+        public int AuthorId { get; set; }
+        public int PostId { get; set; }
+    }
+ 
+    public class EditCommentView
+    {
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+ 
+        public int CommentId { get; set; }
+    }
+ 
+    public class CreateCommentView
+    {
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+    }
+}
